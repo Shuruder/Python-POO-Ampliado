7c860f18ac6632c9b1830406f2df2085efde57ab
shuruder <gabriel_brandini@hotmail.com>
2022-12-29 13:43:37 -0300

    Conceitos de POO de atributos e Metodos

---
+++ 02-poo-metodos.py
@@ -0,0 +1,25 @@
+#   Metodos de classe e estático
+#   Diferenças:
+#   Metodo de Classe recebe o primeiro parametro que aponta pra classe, o estático não
+#   Estático não tem referencia para ser acessada ou alterada (Usado em metodos utilitários)
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+        idade = 2022 - ano
+        return cls(nome, idade)
+
+    @staticmethod
+    def e_maior_idade(idade):
+        return idade >= 18
+
+
+p = Pessoa.criar_de_data_nascimento(1994, 9, 7, "Gabriel")
+print(p.nome, p.idade)
+
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(8))
